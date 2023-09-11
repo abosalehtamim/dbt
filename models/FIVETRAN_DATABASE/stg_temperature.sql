5f9ced3b8489c0370ab6aac9af611ac42bda1a04
Tamim Abo Saleh <tamim.abosaleh@biztory.be>
2023-09-11 15:08:14 +0000

    temperature_change

--- models/FIVETRAN_DATABASE/stg_temperature.sql
+++ models/FIVETRAN_DATABASE/stg_temperature.sql
@@ -3,9 +3,9 @@ WITH WEATHER_DATA AS (
     SELECT
         CASE WHEN LOCATION = 'Schiphol' THEN 'Amsterdam' ELSE LOCATION END AS City,
         TO_DATE(DATE, 'dd/mm/yyyy') AS Date,
-        cast(REPLACE(AVG_TEMPERATURE_C_, ',', '.') as decimal)  AS Avg_temperature,
-        cast(REPLACE(MINIMUM_TEMPERATURE_C_, ',', '.') as decimal) AS Min_temperature,
-        cast(REPLACE(MAXIMUM_TEMPERATURE_C_, ',', '.') as decimal) AS Max_temperature,
+        cast(REPLACE(AVG_TEMPERATURE_C_, ',', '.') as decimal)  AS AVG_TEMPERATURE,
+        cast(REPLACE(MINIMUM_TEMPERATURE_C_, ',', '.') as decimal) AS MINIMUM_TEMPERATURE,
+        cast(REPLACE(MAXIMUM_TEMPERATURE_C_, ',', '.') as decimal) AS MAXIMUM_TEMPERATURE,
         cast(REPLACE(SUN_HOURS, ',', '.') as decimal) as SUN_HOURS
 
     FROM FIVETRAN_DATABASE.WEATHER_DATA1.WEATHER_DATA
